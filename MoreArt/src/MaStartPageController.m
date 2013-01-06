5484e5230619606c06380da9916c1f612abec98d
accthun <accthuN@gmail.com>
2013-01-06 12:01:20 +0800

    LaunchScreen backside images.

--- MoreArt/src/MaStartPageController.m
+++ MoreArt/src/MaStartPageController.m
@@ -36,7 +36,7 @@
         for (int x = 0; x < columns; ++x) {
             UIView *subview = [self.subviews objectAtIndex:index++];
             subview.frame = CGRectMake(x * widthSize, y * heightSize, widthSize, heightSize);
-			//			NSLog(@"SubView frame is %@",NSStringFromCGRect(subview.frame));
+			NSLog(@"SubView frame is %@",NSStringFromCGRect(subview.frame));
         }
     }
 }
@@ -59,45 +59,53 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-		int rows;
-		int columns = MA_START_PAGE_COL;
+	[super viewDidLoad];
+	int rows;
+	int columns = MA_START_PAGE_COL;
+	int backsideImageNO = 1;
+	
+	if ([[UIScreen mainScreen] bounds].size.height > 480) 
+	{
+		rows = MA_START_PAGE_ROWXL	
+	}
+	else
+	{
+		rows = MA_START_PAGE_ROW;
+		backsideImageNO = 5;
+	}
+	
+	subView *containerView = [[subView alloc] init];
+	[self.view addSubview: containerView];
+	containerView.rows = rows;
+	containerView.columns = columns;
+	containerView.contentMode = UIViewContentModeScaleAspectFill;
+
+	for (int i = 0; i < columns * rows; ++i)
+	{
+		subViewController *subController = [[subViewController alloc] init];
+		subController.view.backgroundColor = [UIColor colorWithWhite:50 alpha:0.3];
+
+		NSInteger min = 10; 
+		NSInteger max = 100;
+
+		NSInteger randNum = (arc4random() % (max - min) + min) ; 
+		NSString *numString = [NSString stringWithFormat:@"%d", randNum];
+		NSString *backsideNOString = [NSString stringWithFormat:@"%d", backsideImageNO];
 		
-		if ([[UIScreen mainScreen] bounds].size.height > 480) 
-			rows = MA_START_PAGE_ROWXL	
-		else
-			rows = MA_START_PAGE_ROW;
-		
-		subView *containerView = [[subView alloc] init];
-		[self.view addSubview: containerView];
-		containerView.rows = rows;
-		containerView.columns = columns;
-		containerView.contentMode = UIViewContentModeScaleAspectFill;
-		NSInteger fileSeq = 0;
-		
-		for (int i = 0; i < columns * rows; ++i) {
-			subViewController *subController = [[subViewController alloc] init];
-			subController.view.backgroundColor = [UIColor colorWithWhite:50 alpha:0.3];
-			
-			NSInteger min = 10; 
-			NSInteger max = 100;
-			
-			NSInteger randNum = (arc4random() % (max - min) + min) ; 
-			NSString *numString = [NSString stringWithFormat:@"%d", randNum];
-			NSMutableString* fileName = [NSMutableString stringWithString: @"/launchIMG/squareIMG"];
-			NSMutableString* secondFileName = [NSMutableString stringWithString: @"b"];
-			[fileName appendString:numString];
-			[fileName appendString:@".jpg"];
-			[secondFileName appendString:@".jpeg"];
-			
-			//	NSLog(@"File named %@",fileName);
-			subController.imageName = fileName;
-			subController.secondImageName = secondFileName;
-			[subController setupImage: i];
-			[containerView addSubview:subController.view];
-			
-			fileSeq++;
-		}
+		NSMutableString* fileName = [NSMutableString stringWithString: @"/launchIMG/squareIMG"];
+		NSMutableString* backsideName = [NSMutableString stringWithString: @"/launchIMG/backside"];
+		[fileName appendString:numString];
+		[fileName appendString:@".jpg"];
+		[backsideName appendString:backsideNOString];
+		[backsideName appendString:@".png"];
+
+		NSLog(@"File named %@",backsideName);
+		subController.imageName = fileName;
+		subController.secondImageName = backsideName;
+		[subController setupImage: i];
+		[containerView addSubview:subController.view];
+		backsideImageNO++;
+	}
 }
 
 -(void) viewDidAppear:(BOOL)animated
